f46a2dfc525ec3fc6d06883d3e4f269a2bc270af
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-04-21 10:06:13 +0530

    Creating pages and adding header component to home page

---
+++ Shopping-app/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import Header from "../../components/Header/Header"
+import "./home.css"
+export default function Home(){
+    return(
+        <div className="home">
+            <Header/>
+        </div>
+    )
+}
