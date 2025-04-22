153f5132c1f39cdaeb8452bef33c4d8a9e307027
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-04-22 12:03:51 +0530

    Creating feature section in the shopping app

--- Shopping-app/src/assets/asset.js
+++ Shopping-app/src/assets/asset.js
@@ -1,5 +1,38 @@
-import logo from "./logo.png"
+import logo from "./logo.png";
+import f1 from "./f1.png";
+import f2 from "./f2.png";
+import f3 from "./f3.png";
+import f4 from "./f4.png";
+import f5 from "./f5.png";
+import f6 from "./f6.png";
 
 export const assets ={
     logo
 }
+
+export const feature =[
+    {
+        image: f1,
+        title : "Free Shipping"
+    },
+    {
+        image: f2,
+        title : "Online Order"
+    },
+    {
+        image: f3,
+        title : "Save Money"
+    },
+    {
+        image: f4,
+        title : "Promotion"
+    },
+    {
+        image: f5,
+        title : "Happy Sell"
+    },
+    {
+        image: f6,
+        title : "F24/7 Support"
+    },
+]
--- Shopping-app/src/pages/Home/Home.jsx
+++ Shopping-app/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Feature from "../../components/Feature/Feature"
 import Header from "../../components/Header/Header"
 import "./home.css"
 export default function Home(){
     return(
         <div className="home">
             <Header/>
+            <Feature/>
         </div>
     )
 }
